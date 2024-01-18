6456da9b93cecb7fef4474cdd3267114167b3f4e
timofejsgrisjuks231RDB114 <144841404+timofejsgrisjuks@users.noreply.github.com>
2024-01-18 13:35:30 +0000

    15:35

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
-
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+service = Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=option)
+url = "https://estudijas.rtu.lv/my/"
+driver.get(url)
+time.sleep(2)
+find = driver.find_element(By.ID, "submit")
+find.click()
+time.sleep(3)
