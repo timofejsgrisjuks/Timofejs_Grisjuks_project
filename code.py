bd87b2139da8945c88a40e189003abb525bfaebb
timofejsgrisjuks231RDB114 <144841404+timofejsgrisjuks@users.noreply.github.com>
2024-01-18 14:09:35 +0000

    16:08

--- code.py
+++ code.py
@@ -2,7 +2,10 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+print ("password!")
+password = input()
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
@@ -11,4 +14,12 @@ driver.get(url)
 time.sleep(2)
 find = driver.find_element(By.ID, "submit")
 find.click()
-time.sleep(3)
+time.sleep(2)
+find = driver.find_element(By.ID, "IDToken1")
+find.send_keys("timofejs.grisjuks")
+time.sleep(1)
+find = driver.find_element(By.ID, "IDToken2")
+find.send_keys(password)
+time.sleep(1)
+webdriver.ActionChains(driver).send_keys(Keys.RETURN).perform()
+time.sleep(2)
